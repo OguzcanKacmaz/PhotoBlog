57512d98b55fd6a6d8a9fc9720c1f972789406bd
Mustafa Oğuzcan Kaçmaz <oguzcankacmaz@hotmail.com>
2023-01-16 17:03:56 +0300

    Add project files.

---
+++ PhotoBlog/Areas/Admin/Models/EditViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using PhotoBlog.Attributes;
+
+namespace PhotoBlog.Areas.Admin.Models
+{
+    public class EditViewModel
+    {
+        public int Id { get; set; }
+
+        [MaxLength(140)]
+        public string Title { get; set; } = null!;
+
+        [MaxLength(400)]
+        public string? Description { get; set; }
+
+        [ValidImage(MaxFileSize = 2)]
+        public IFormFile? Photo { get; set; }
+
+
+        public HashSet<string>? Tags { get; set; } = new HashSet<string>();
+    }
+}
---
+++ PhotoBlog/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PhotoBlog.Data;
+using PhotoBlog.Models;
+
+namespace PhotoBlog.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _db;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext db)
+        {
+            _logger = logger;
+            _db = db;
+        }
+
+        public IActionResult Index(string? q)
+        {
+            IQueryable<Post> query = _db.Posts
+                .Include(x => x.Tags);
+
+            if (!string.IsNullOrWhiteSpace(q))
+            {
+                q = q.Trim();
+
+                if (q.StartsWith("#"))
+                {
+                    var tag = q.Substring(1);
+                    query = query.Where(x => x.Tags.Any(t => t.Name == tag));
+                }
+                else
+                {
+                    query = query.Where(x => 
+                        x.Title.Contains(q) || x.Description!.Contains(q));
+                }
+            }
+
+            var posts = query
+                .OrderByDescending(x => x.CreatedTime)
+                .ToList();
+
+            var vm = new HomeViewModel()
+            {
+                Posts = posts,
+                Search = q
+            };
+
+            return View(vm);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
